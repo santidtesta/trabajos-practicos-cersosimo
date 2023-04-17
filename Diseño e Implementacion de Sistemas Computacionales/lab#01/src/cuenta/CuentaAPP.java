989fa5361dfaa38d645fc1b57835df2f12963746
santino <santinodonatotesta5to4ta@gmail.com>
2023-04-16 22:18:27 -0300

    carpeta reorganizadas

---
+++ Diseño e Implementacion de Sistemas Computacionales/lab#01/src/cuenta/CuentaAPP.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuenta;
+
+public class CuentaAPP {
+    public static void main(String[] args) {
+        Cuenta c1 = new Cuenta("Pepe", "0192857", 7.0, 100000);
+        
+    }
+   
+}
